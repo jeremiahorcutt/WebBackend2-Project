0fc97d8bfe72b2bf0cac25ae038dbefa6a80b93b
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-09-15 16:41:13 -0600

    user display after creation

--- cse341-project-master/prove01-routes.js
+++ cse341-project-master/prove01-routes.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-
+const { runInNewContext } = require('vm');
+const finalUsers = [];
 
 const rqHandler = (req, res) => {  
         const url = req.url;
@@ -15,23 +16,29 @@ const rqHandler = (req, res) => {
             res.setHeader('Content-Type', 'text/html');
             res.write('<html>');
             res.write('<head><title>Problem 1</title></head>');
-            res.write('<bodt><ul><li>User1</li><li>User2</li></ul></body>');
+            res.write('<bodt><ul>');
+            finalUsers.forEach(user => res.write('<li>'+user+'</li>'));
+            res.write('</ul></body>');
             res.write('</html>');
             return res.end();
         }
         if(url === '/create-user') {
             const body = [];
+            const users = [];
             req.on('data', chunk => {
                 body.push(chunk);
+                users.push(chunk);
             });
             req.on('end', () => {
                 const parsedBody = Buffer.concat(body).toString();
+                const parsedUsers = Buffer.concat(users).toString();
+                refinedUsers = parsedUsers.split('=')[1];
                 console.log(parsedBody.split('=')[1]);
+                finalUsers.push(refinedUsers);
             });
             res.statusCode = 302;
             res.setHeader('Location', '/');
             res.end();
         }
     };
-
     module.exports = rqHandler;
