3472700baafe88b6dde9ab6075e52a51f3cba6b5
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-09-19 17:47:23 -0600

    made styling changes

--- cse341-project-master/prove02/routes/admin.js
+++ cse341-project-master/prove02/routes/admin.js
@@ -10,11 +10,16 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product', path:'/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true});
+    res.render('add-product', {
+      pageTitle: 'Add Product', 
+      path:'/admin/add-product', 
+      formsCSS: true, 
+      productCSS: true, 
+      activeAddProduct: true});
   });
   
 router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
+    products.push({title: req.body.title, price: req.body.price, genre: req.body.genre, desc: req.body.desc});
     res.redirect('/');
   });
 
