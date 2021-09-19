eba942554e2eba8c307f92edf0392dd5447e77e5
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-09-19 16:46:54 -0600

    added prove02 to package

--- cse341-project-master/prove02/prove02-routes/admin.js
+++ cse341-project-master/prove02/prove02-routes/admin.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const rootDir = require('../prove02-util/path');
 const { runInNewContext } = require('vm');
 
 const router = express.Router();
