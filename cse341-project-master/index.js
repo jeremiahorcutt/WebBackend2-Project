90bc25d6a04958e65545ce12fc4ac6b82c73a186
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-09-22 09:51:46 -0600

    a

--- cse341-project-master/index.js
+++ cse341-project-master/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localh
 const app = express();
 
 // Route setup. You can implement more in the future!
-const ta01Routes = require('./routes/ta01');
+const ta01Routes = require('./routes/ta011');
 const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04');
@@ -35,7 +35,7 @@ app
   //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
   //.set('view engine', 'hbs')
   .use(bodyParser({ extended: false })) // For parsing the body of a POST
-  .use('/ta021', ta01Routes)
+  .use('/ta01', ta01Routes)
   .use('/ta02', ta02Routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
